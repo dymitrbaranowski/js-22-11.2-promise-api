7833f8454c782814322401817c78439ae8b3f71f
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-10-25 10:38:27 +0300

    fox1

---
+++ src/js/01-promise-api.js
@@ -0,0 +1,87 @@
+/*
+ * Создание промиса
+ *  - Класс Promise
+ *  - resolve
+ *  - reject
+ *  - Promise.prototype.then(onResolve, onReject)
+ */
+
+const promise = new Promise((resolve, reject) => {
+  const canFulfill = Math.random() > 0.5;
+
+  setTimeout(() => {
+    if (canFulfill) {
+      resolve('Промис выполнился успешно, с результатом (исполнен, fulfilled)');
+    }
+    reject('Промис выполнился с ошибкой (отклонён, rejected)');
+  }, 1000);
+});
+
+// promise.then(onFullFilled, onRejected);
+
+function onFulfilled(result) {
+  console.log('onFulfilled -> onFulfilled');
+  console.log(`✅ ${result}`);
+}
+
+function onRejected(error) {
+  console.log('onRejected -> onRejected');
+  console.log(`❌ ${error}`);
+}
+
+promise
+  .then(onFulfilled)
+  .then(x => {
+    console.log(x);
+
+    throw new Error('Error in second then');
+
+    return 10;
+  })
+  .then(y => {
+    console.log(y);
+  })
+  .catch(error => console.log(error));
+
+// const promise = new Promise((resolve, reject) => {
+//   const canFulfill = Math.random() > 0.5;
+
+//   setTimeout(() => {
+//     if (canFulfill) {
+//       resolve('Промис выполнился успешно, с результатом (исполнен, fulfilled)');
+//     }
+
+//     reject('Промис выполнился с ошибкой (отклонён, rejected)');
+//   }, 1000);
+// });
+
+// promise.then(onFulfilled, onRejected);
+
+// function onFulfilled(result) {
+//   console.log('onFulfilled -> onFulfilled');
+//   console.log(`✅ ${result}`);
+// }
+
+// function onRejected(error) {
+//   console.log('onRejected -> onRejected');
+//   console.log(`❌ ${error}`);
+// }
+
+/*
+ * Цепочки промисов (chaining)
+ * Promise.prototype.catch(error)
+ * Promise.prototype.finally()
+ */
+
+// promise
+//   .then(onFulfilled)
+//   .then(x => {
+//     console.log(x);
+
+//     return 10;
+//   })
+//   .then(y => {
+//     console.log(y);
+//   })
+//   .catch(error => console.log(error))
+//   .finally(() => console.log('Я буду выполнен в любом случае'));
