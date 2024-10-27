45a5d0f3bb0fa4f841e45d1810289b681b15208c
dymitrbaranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-10-27 18:33:43 +0000

    Deploying to gh-pages from @ dymitrbaranowski/js-22-11.2-promise-api@d1f4adc886c7f6e2f3e9bd15b8d16ae9c441fd40 🚀

--- 03-racetrack.e5ee6d67.js
+++
@@ -1,2 +0,0 @@
-!function(){var n;(n="Mango",new Promise((function(o){var t,e,r=(t=2e3,e=3500,Math.floor(Math.random()*(e-t+1)+t));setTimeout((function(){o({horse:n,time:r})}),r)}))).then((function(n){return console.log(n)})).catch((function(n){return console.log(n)}))}();
-//# sourceMappingURL=03-racetrack.e5ee6d67.js.map
